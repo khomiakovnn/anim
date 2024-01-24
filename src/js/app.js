3cefc0cb7f4cb8735b9bc350b2a390477ba78404
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2024-01-24 15:55:05 -0500

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-console.log('app worked');
+document.addEventListener('DOMContentLoaded', function () {
+    var collapsibleBtn = document.querySelector('.collapsible-btn');
+    var collapsibleBox = document.querySelector('.collapsible-box');
+
+    collapsibleBtn.addEventListener('click', function () {
+        if (collapsibleBox.style.maxHeight) {
+            collapsibleBox.style.maxHeight = null;
+        } else {
+            collapsibleBox.style.maxHeight = collapsibleBox.scrollHeight + "px";
+        }
+    });
+});
+
+
